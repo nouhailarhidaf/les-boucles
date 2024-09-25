4c61c4b4008f3835b846bfcc01eb8dc07e978462
nouhaila-rhidaf <nouhailarhidaf04@gmail.com>
2024-09-25 17:34:52 +0100

    les boucles

---
+++ cha3 les boucles.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+   int ab,c,N,somme;
+
+   printf("veuillez entrer deux nombres à additionner\n");
+   scanf("%d%d%d%d", &a,&b,&c,&N);
+   somme = a+b+c+N;
+
+   printf("La somme des deux nombres est = %d\n", somme);
+
+   return 0;
+}
